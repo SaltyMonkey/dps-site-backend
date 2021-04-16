085ec1f873b008e42b9fae7c7fd99b818f25695f
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2021-04-16 13:47:48 +0300

    fixed search

--- server/models/uploadModel.js
+++ server/models/uploadModel.js
@@ -52,22 +52,94 @@ const upload = new mongoose.Schema({
 
 upload.plugin(require("mongoose-autopopulate"));
 
-upload.statics.getLatestRuns = async function (searchParams, amount) {
-	let runs = [];
-	runs = await this.find(searchParams, {
-		"runId": 1,
-		"huntingZoneId": 1,
-		"bossId": 1,
-		"encounterUnixEpoch": 1,
-		"fightDuration": 1,
-		"isP2WConsums": 1,
-		"isMultipleTanks": 1,
-		"isMultipleHeals": 1,
-		"partyDps": 1,
-		"members.playerDps": 1,
-		"members.userData": 1
-	}).sort({ "encounterUnixEpoch": -1 }).limit(amount).lean({ autopopulate: true });
-	return runs || [];
+upload.statics.getLatestRuns = async function (data, amount) {
+	let matchStage = {
+		$match: {
+			region: data.region
+		}
+	};
+
+	if(data.encounterUnixEpoch) matchStage["$match"].encounterUnixEpoch = data.encounterUnixEpoch;
+	if(data.bossId) matchStage["$match"].bossId = data.bossId;
+	if(data.huntingZoneId) matchStage["$match"].huntingZoneId = data.huntingZoneId;
+	// eslint-disable-next-line no-prototype-builtins
+	if(data.hasOwnProperty("isShame")) matchStage["$match"].isShame = data.isShame;
+	// eslint-disable-next-line no-prototype-builtins
+	if(data.hasOwnProperty("isP2WConsums")) matchStage["$match"].isP2WConsums = data.isP2WConsums;
+	// eslint-disable-next-line no-prototype-builtins
+	if(data.hasOwnProperty("isMultipleHeals")) matchStage["$match"].isShame = data.isShame;
+	// eslint-disable-next-line no-prototype-builtins
+	if(data.hasOwnProperty("isMultipleTanks")) matchStage["$match"].isP2WConsums = data.isP2WConsums;
+	if(data.roleType) matchStage["$match"]["members.roleType"] = data.roleType;
+
+	let postMatchStep = { $elemMatch: {}};
+
+	if(data.playerName) postMatchStep["$elemMatch"].playerClass = data.playerClass;
+	if(data.playerServer) postMatchStep["$elemMatch"].playerServer = data.playerServer;
+	if(data.playerClass) postMatchStep["$elemMatch"].playerClass = data.playerClass;
+
+	let agregationTemplate = [
+		{
+			"$project": {
+				"region": 1,
+				"runId": 1,
+				"bossId": 1,
+				"huntingZoneId": 1,
+				"encounterUnixEpoch": 1,
+				"fightDuration": 1,
+				"isP2WConsums": 1,
+				"isMultipleHeals": 1,
+				"isMultipleTanks": 1,
+				"isShame": 1,
+				"members.userData": 1,
+				"members.playerDps": 1,
+				"members.roleType": 1
+			}
+		},
+		matchStage,
+		{
+			"$unwind": "$members"
+		},
+		{
+			"$lookup": {
+				"from": "players",
+				"localField": "members.userData",
+				"foreignField": "_id",
+				"as": "player"
+			}
+		},
+		{
+			$unwind: "$player"
+		},
+		{
+			$addFields: {
+				"members.playerClass": "$player.playerClass",
+				"members.playerName": "$player.playerName",
+				"members.playerServer": "$player.playerServer"
+			}
+		},
+		{
+			$group: {
+				_id: "$_id", 
+				members: { $push: "$members"},
+				bossId: { $first: "$bossId"},
+				debuffDetail: { $first: "$debuffDetail"},
+				encounterUnixEpoch: { $first: "$encounterUnixEpoch"},
+				huntingZoneId: { $first: "$huntingZoneId"},
+				isShame: { $first: "$isShame" },
+				isMultipleTanks: { $first: "$isMultipleTanks" },
+				isMultipleHeals: { $first: "$isMultipleHeals" },
+				isP2WConsums: { $first: "$isP2WConsums" },
+				runId: { $first: "$runId" },
+				region: { $first: "$region" },
+				fightDuration: { $first: "$fightDuration" },
+				partyDps: { $first: "$partyDps" },
+			}
+		}
+	];
+
+	if(Object.keys(postMatchStep["$elemMatch"]) > 0) agregationTemplate.push(postMatchStep);
+	return await this.aggregate(agregationTemplate).sort({ "encounterUnixEpoch": -1 }).limit(amount);
 };
 
 upload.statics.getCompleteRun = async function (id) {
